de598d8c5ec3ae7f1dbccf0b16456ad9270c3d17
Krys Flores <krys@lob.com>
2020-12-14 21:30:22 -0800

    Add comments

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
+# Docker creates a computer container
+# this tells Docker to get node version 14
 FROM node:14
+# this tells Docker that I want my directory within the computer
+# container to be called /app
 WORKDIR /app
+# node uses package.json to manage packages
+# this line says, copy my package files to the computer container
 COPY package*.json ./
+# the next line installs all of our packages
 RUN npm install
+# puts all of our files from lob-love-letters into the computer container directory /app
 COPY . .
+
+# in docker-compose.yml, we name our computer container "lob-love-letters"
--- send_cats_postcard.js
+++ send_cats_postcard.js
@@ -1,10 +1,17 @@
+// this tells Lob which environment to use: ex. TEST or LIVE
+// this is coming from your .env file
 const LOB_API_KEY = process.env.LOB_API_KEY;
+// https://docs.lob.com/ and look at the Node section
+// this is how you include Lob and send thing to Lob
 const Lob = require('lob')(LOB_API_KEY);
 
-const fs = require('fs');
+// read my file, please
+// OLD const fs = require('fs');
+import { createReadStream } from 'fs'; // ES6 or ES2015: how to write your code, neatly
 
 console.log(`INSIDE PREP POSTCARD`);
 
+// TO ADDRESS
 const FRIENDS_ADDRESS = {
     name: 'Lara Lobster',
     address_line1: '210 King St',
@@ -14,6 +21,7 @@ const FRIENDS_ADDRESS = {
     address_zip: '94107'
 }
 
+// FROM ADDRESS
 const MY_ADDRESS = {
     name: 'Lena Lobster',
     address_line1: '210 King St',
@@ -23,6 +31,8 @@ const MY_ADDRESS = {
     address_zip: '94107'
 }
 
+// MERGE VARIABLES
+// These variables help you make things personal
 const recipient = 'Lara';
 const message = "You're the best. Thanks for making my 2020 better!";
 const closing = 'Hugs,';
@@ -32,11 +42,11 @@ Lob.postcards.create({
     description: 'Lob Love Letters Cats Postcard',
     to: FRIENDS_ADDRESS,
     from: MY_ADDRESS,
-    front: fs.createReadStream('./assets/CatsPostcardFront.html'),
-    back: fs.createReadStream('./assets/CatsPostcardBack.html'),
+    front: createReadStream('./assets/CatsPostcardFront.html'),
+    back: createReadStream('./assets/CatsPostcardBack.html'),
     merge_variables: {
-        recipient,
-        message,
+        recipient, // recipient: recipient 
+        message, 
         closing,
         sender
     }
